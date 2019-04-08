621ac1d8772a582f353f4c7f99f2499601e3df42
Ari Madian <akmadian@gmail.com>
2019-04-07 23:13:22 -0700

    Standardize license_xsl/licensexsl_tools/licenses.py

--- license_xsl/licensexsl_tools/licenses.py
+++ license_xsl/licensexsl_tools/licenses.py
@@ -28,37 +28,37 @@ def makeOpts():
     usage = "usage: %prog [options] [<version>] [<jurisdiction>]"
     parser = OptionParser(usage)
 
-    parser.add_option( '-i', '--info',    dest='infoOnly',
+    parser.add_option( "-i", "--info",    dest="infoOnly",
                        action="store_true",
                        help="Only add jurisdiction information, not licenses.",
                        )
 
-    parser.add_option( '-f', '--file', dest='filename',
-                       help='Specify the file to update (defaults '
-                       'to licenses.xml)')
-    parser.add_option( '-c', '--codes', dest='licenses',
-                       help='License codes to add, comma delimited '
-                       '(defaults to primary six)',
+    parser.add_option( "-f", "--file", dest="filename",
+                       help="Specify the file to update (defaults "
+                       "to licenses.xml)")
+    parser.add_option( "-c", "--codes", dest="licenses",
+                       help="License codes to add, comma delimited "
+                       "(defaults to primary six)",
                        )
 
     # jurisdiction information options
-    parser.add_option( '--launched', dest='launched',
+    parser.add_option( "--launched", dest="launched",
                        action="store_true",
                        help="Mark the specified jurisdiction as launched.",
                        )
-    parser.add_option( '--not-launched', dest='launched',
-                       action='store_false',
+    parser.add_option( "--not-launched", dest="launched",
+                       action="store_false",
                        help="Mark the specified jurisdiction as not launched.",
                        )
-    parser.add_option( '--lang', dest='langs',
+    parser.add_option( "--lang", dest="langs",
                        help="Comma delimited list of languages for the "
                        "specified jurisdiction",
                        )
-    parser.add_option( '--uri', dest='juris_uri',
+    parser.add_option( "--uri", dest="juris_uri",
                        help="The URI of the jurisdiction specific web page.",
                        )
     
-    parser.set_defaults(filename='licenses.xml',
+    parser.set_defaults(filename="licenses.xml",
                         licenses="by-nc,by,by-nc-nd,by-nc-sa,by-sa,by-nd",
                         infoOnly=False,
                         langs=[])
@@ -92,8 +92,8 @@ def checkArgs(opts, args):
             print "Ambiguous version arguments: %s" % ",\n ".join(version_arg)
             return None
         else:
-            result['version'] = version_arg[0]
-            remaining.remove(result['version'])
+            result["version"] = version_arg[0]
+            remaining.remove(result["version"])
 
     # should be one remaining argument, the jurisdiction
     if len(remaining) > 1:
@@ -101,7 +101,7 @@ def checkArgs(opts, args):
               ",\n ".join(remaining)
         return None
 
-    result['jurisdiction'] = remaining[0]
+    result["jurisdiction"] = remaining[0]
 
     return result
 
@@ -110,54 +110,54 @@ def checkArgs(opts, args):
 
 def addLicense(filename, jurisdiction, version, licenses):
 
-    #Let's begin. First check: Find the six active licenses.
+    #Let"s begin. First check: Find the six active licenses.
 
     tree=etree.ElementTree(file=filename)
     root=tree.getroot()
     
-    firstit=root.getiterator('license')
+    firstit=root.getiterator("license")
     for v in firstit:
         found=0
-        if v.get('id') in licenses:
+        if v.get("id") in licenses:
             #When you find an active license, 
             # now check for an existing jurisdiction.
-            print "Adding to "+v.get('id')+" license..."
-            tempit=v.getiterator('jurisdiction')
+            print "Adding to "+v.get("id")+" license..."
+            tempit=v.getiterator("jurisdiction")
             for w in tempit:
-                if w.get('id')==jurisdiction:
-                    #If it's there, add in the new version.
+                if w.get("id")==jurisdiction:
+                    #If it"s there, add in the new version.
                     found=1
                     print "Adding version %s to existing jurisdiction %s." % \
                           (version, jurisdiction)
-                    newelem=w.makeelement('version',None)
-                    newelem.set('id',version)
-                    j=''
-                    if jurisdiction!='-':
-                        j=jurisdiction+'/'
-                    newelem.set('uri',"http://creativecommons.org/licenses/"+v.get('id')+'/'+version+'/'+j)
-                    newelem.tail='\n'
+                    newelem=w.makeelement("version",None)
+                    newelem.set("id",version)
+                    j=""
+                    if jurisdiction!="-":
+                        j=jurisdiction+"/"
+                    newelem.set("uri","http://creativecommons.org/licenses/"+v.get("id")+"/"+version+"/"+j)
+                    newelem.tail="\n"
                     w.append(newelem)
             if found==0:
-                #If it's not there, add a new jurisdiction too.
+                #If it"s not there, add a new jurisdiction too.
                 print "Creating new jurisdiction "+jurisdiction
-                newelem=v.makeelement('jurisdiction',None)
-                newelem.set('id',jurisdiction)
-                newelem.text='\n'
+                newelem=v.makeelement("jurisdiction",None)
+                newelem.set("id",jurisdiction)
+                newelem.text="\n"
                 v.append(newelem)
                 print "Adding version "+version+" to new jurisdiction "+jurisdiction
-                newelem2=newelem.makeelement('version',None)
-                newelem2.set('id',version)
-                j=''
-                if jurisdiction!='-':
-                    j=jurisdiction+'/'
-                newelem2.set('uri',"http://creativecommons.org/licenses/"+v.get('id')+'/'+version+'/'+j)
-                newelem2.tail='\n'
+                newelem2=newelem.makeelement("version",None)
+                newelem2.set("id",version)
+                j=""
+                if jurisdiction!="-":
+                    j=jurisdiction+"/"
+                newelem2.set("uri","http://creativecommons.org/licenses/"+v.get("id")+"/"+version+"/"+j)
+                newelem2.tail="\n"
                 newelem.append(newelem2)
 
 
 
     #And write the new file.
-    fileopen=open(filename,'w')
+    fileopen=open(filename,"w")
     tree.write(fileopen)
 
 def addInfo(filename, jurisdiction, uri, languages, launched):
@@ -165,18 +165,18 @@ def addInfo(filename, jurisdiction, uri, languages, launched):
     tree=etree.ElementTree(file=filename)
     root=tree.getroot()
 
-    j_node = tree.xpath('/license-info/jurisdictions/jurisdiction-info')
+    j_node = tree.xpath("/license-info/jurisdictions/jurisdiction-info")
     j_node = [n for n in j_node
               if n.xpath('./@id="%s"' % jurisdiction)]
 
-    # check if the jurisdiction doesn't exist at all yet
+    # check if the jurisdiction doesn"t exist at all yet
     if len(j_node) == 0:
         # add the jurisdiction node, defaulting to not launched
         print "Adding jurisdiction-info for %s..." % jurisdiction
-        jurisdiction_parent = tree.xpath('/license-info/jurisdictions')[0]
-        j_node = jurisdiction_parent.makeelement('jurisdiction-info',
-                                                 {'launched':'false'})
-        j_node.set('id', jurisdiction)
+        jurisdiction_parent = tree.xpath("/license-info/jurisdictions")[0]
+        j_node = jurisdiction_parent.makeelement("jurisdiction-info",
+                                                 {"launched":"false"})
+        j_node.set("id", jurisdiction)
         
         jurisdiction_parent.append(j_node)
     else:
@@ -186,38 +186,38 @@ def addInfo(filename, jurisdiction, uri, languages, launched):
     if launched is not None:
         print "Updating launched status for %s..." % jurisdiction
         
-        j_node.set('launched', str(launched).lower())
+        j_node.set("launched", str(launched).lower())
         
     if languages != []:
         print "Updating languages for %s..." % jurisdiction
         
-        lang_nodes = j_node.xpath('./languages')
+        lang_nodes = j_node.xpath("./languages")
         if len(lang_nodes) > 0:
             # a languages node already exists, remove it
             for ln in lang_nodes:
                 j_node.remove(ln)
                 
         # make a new languages node
-        lang_node = j_node.makeelement('languages', None)
+        lang_node = j_node.makeelement("languages", None)
         lang_node.text = languages
         j_node.append(lang_node)
 
     if uri is not None:
         print "Updating URI for %s..." % jurisdiction
         
-        uri_nodes = j_node.xpath('./uri')
+        uri_nodes = j_node.xpath("./uri")
         if len(uri_nodes) > 0:
             # a languages node already exists, remove it
             for u_node in uri_nodes:
                 j_node.remove(u_node)
                 
         # make a new languages node
-        uri_node = j_node.makeelement('uri', None)
+        uri_node = j_node.makeelement("uri", None)
         uri_node.text = uri
         j_node.append(uri_node)
     
     #And write the new file.
-    fileopen=open(filename,'w')
+    fileopen=open(filename,"w")
     tree.write(fileopen)
 
 def main():
@@ -234,15 +234,15 @@ def main():
         
     if not(opts.infoOnly):
         addLicense(opts.filename,
-                   argdict['jurisdiction'],
-                   argdict['version'],
-                   opts.licenses.split(','))
+                   argdict["jurisdiction"],
+                   argdict["version"],
+                   opts.licenses.split(","))
     else:
         addInfo(opts.filename,
-                argdict['jurisdiction'],
+                argdict["jurisdiction"],
                 opts.juris_uri,
                 opts.langs,
                 opts.launched)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
