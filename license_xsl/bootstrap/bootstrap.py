ec0c6bbb9b4e46b4e6121b60d9cecffeb3408809
Ari Madian <akmadian@gmail.com>
2019-04-07 23:41:25 -0700

    Standardize license_xsl/bootstrap/boostrap.py

--- license_xsl/bootstrap/bootstrap.py
+++ license_xsl/bootstrap/bootstrap.py
@@ -24,21 +24,21 @@ import os, shutil, sys, tempfile, urllib2
 
 tmpeggs = tempfile.mkdtemp()
 
-is_jython = sys.platform.startswith('java')
+is_jython = sys.platform.startswith("java")
 
 try:
     import pkg_resources
 except ImportError:
     ez = {}
-    exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
+    exec urllib2.urlopen("http://peak.telecommunity.com/dist/ez_setup.py"
                          ).read() in ez
-    ez['use_setuptools'](to_dir=tmpeggs, download_delay=0)
+    ez["use_setuptools"](to_dir=tmpeggs, download_delay=0)
 
     import pkg_resources
 
-if sys.platform == 'win32':
+if sys.platform == "win32":
     def quote(c):
-        if ' ' in c:
+        if " " in c:
             return '"%s"' % c # work around spawn lamosity on windows
         else:
             return c
@@ -49,36 +49,36 @@ else:
 cmd = 'from setuptools.command.easy_install import main; main()'
 ws  = pkg_resources.working_set
 
-if len(sys.argv) > 2 and sys.argv[1] == '--version':
-    VERSION = ' == %s' % sys.argv[2]
-    args = sys.argv[3:] + ['bootstrap']
+if len(sys.argv) > 2 and sys.argv[1] == "--version":
+    VERSION = " == %s" % sys.argv[2]
+    args = sys.argv[3:] + ["bootstrap"]
 else:
-    VERSION = ''
-    args = sys.argv[1:] + ['bootstrap']
+    VERSION = ""
+    args = sys.argv[1:] + ["bootstrap"]
 
 if is_jython:
     import subprocess
 
-    assert subprocess.Popen([sys.executable] + ['-c', quote(cmd), '-mqNxd',
-           quote(tmpeggs), 'zc.buildout' + VERSION],
+    assert subprocess.Popen([sys.executable] + ["-c", quote(cmd), "-mqNxd",
+           quote(tmpeggs), "zc.buildout" + VERSION],
            env=dict(os.environ,
                PYTHONPATH=
-               ws.find(pkg_resources.Requirement.parse('setuptools')).location
+               ws.find(pkg_resources.Requirement.parse("setuptools")).location
                ),
            ).wait() == 0
 
 else:
     assert os.spawnle(
         os.P_WAIT, sys.executable, quote (sys.executable),
-        '-c', quote (cmd), '-mqNxd', quote (tmpeggs), 'zc.buildout' + VERSION,
+        "-c", quote (cmd), "-mqNxd", quote (tmpeggs), "zc.buildout" + VERSION,
         dict(os.environ,
             PYTHONPATH=
-            ws.find(pkg_resources.Requirement.parse('setuptools')).location
+            ws.find(pkg_resources.Requirement.parse("setuptools")).location
             ),
         ) == 0
 
 ws.add_entry(tmpeggs)
-ws.require('zc.buildout' + VERSION)
+ws.require("zc.buildout" + VERSION)
 import zc.buildout.buildout
 zc.buildout.buildout.main(args)
 shutil.rmtree(tmpeggs)
